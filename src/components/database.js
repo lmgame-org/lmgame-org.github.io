2dd4e7161436d47a975355f3a00c4c694615d074
Yuxuan <yuxuan_zhang13@outlook.com>
2024-10-30 19:04:38 -0700

    update leaderboard template

---
+++ src/components/database.js
@@ -0,0 +1,8 @@
+export const Leaderboard = [
+    { name: "Shawn Hanna", score: 1550 },
+    { name: "Fidel Hand", score: 2310 },
+    { name: "Bessie Hickle", score: 350 },
+    { name: "Adella Wunsch", score: 2100 },
+    { name: "Clair O'Connell", score: 1250 },
+    { name: "Kameron Prosacco", score: 5250 }
+];
