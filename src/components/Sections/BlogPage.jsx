cfb5ed945954c334509806535ef8d3f7c69780bc
Your Name <vica031106@gmail.com>
2024-12-07 16:22:29 -0800

    edit icon and edit blog

--- src/components/Sections/BlogPage.jsx
+++ src/components/Sections/BlogPage.jsx
@@ -1,14 +1,32 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import MarkdownRenderer from "./MarkdownRenderer";
+import TopNavbar from "./Pages/TopNavbar_pages";
+import Footer from "./Footer";
+
+import styled from "styled-components";
 
 export default function BlogPage() {
   const { blogId } = useParams();
   const filePath = `/blogs/${blogId}.md`;
 
   return (
-    <div style={{ padding: "20px" }}>
-      <MarkdownRenderer filePath={filePath} />
-    </div>
+
+    <>
+    <TopNavbar />
+    <div id="top"></div>
+    <Wrapper id="blog">
+      <div className="whiteBg" style={{ padding: "90px 0" }}>
+      <div style={{ padding: "20px" }}>
+        <MarkdownRenderer filePath={filePath} />
+      </div>
+      </div>
+    </Wrapper>
+    <Footer />
+    </>
   );
 }
+
+const Wrapper = styled.section`
+  width: 100%;
+`;
