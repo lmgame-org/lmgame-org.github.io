481a7095b722b92f41208f65a7eab429b633fc04
Lanxiang Hu <hlxde1@yahoo.com>
2025-06-30 17:53:26 +0000

    finalize blogpost: title

--- src/components/Sections/Blog.jsx
+++ src/components/Sections/Blog.jsx
@@ -37,7 +37,7 @@ export default function Projects() {
               <StyledImage src={PokemonRedCover} alt="pokemon_red" />
             </ImageWrapper>
             <TextWrapper>
-              <PostTitle onClick={() => navigate("/blog/pokemon_red")}>How Good is Pokémon Red as a LLM Eval?</PostTitle>
+              <PostTitle onClick={() => navigate("/blog/pokemon_red")}>From Pokémon Red to Standardized Game-as-Eval</PostTitle>
               <PostDescription>
                 <TLDR onClick={() => togglePostDescription(0)}>
                   {expandedPost === 0 ? "Hide TLDR" : "Show TLDR"}
